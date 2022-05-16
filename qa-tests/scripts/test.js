bc42b4c571820e77e6c0d77e8f0731bf9db5c745
Koushik GRY <koushik.gurijalareddyyachendra@telus.com>
2022-05-16 02:22:40 -0400

    Adding few files

--- qa-tests/scripts/test.js
+++ qa-tests/scripts/test.js
@@ -4,7 +4,7 @@ import { sleep } from 'k6';
 
 export const options = {
   duration: '1m',
-  vus: 50,
+  vus: 20,
   thresholds: {
     http_req_failed: ['rate<0.01'],
     http_req_duration: ['p(95)<500'],
@@ -12,6 +12,6 @@ export const options = {
 };
 
 export default function () {
-  const res = http.get('https://test.k6.io');
+  const res = http.get('http://104.198.74.49:8080');
   sleep(1);
 }
