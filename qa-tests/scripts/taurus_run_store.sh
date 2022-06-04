7e2e2f98c447ef077604865792a702309826cc6b
csduvvuri <csduvvuri@gmail.com>
2022-06-05 02:03:26 +0530

    taurus testing

---
+++ qa-tests/scripts/taurus_run_store.sh
@@ -0,0 +1,9 @@
+echo "Currently running bzt command in Container"
+bzt /scripts/test.yml
+date_now=$(date "+%F-%H-%M-%S")
+echo "The date is $date_now"
+mv /tmp/artifacts /tmp/artifacts-$date_now
+echo "Script for copying the artifacts to Cloud Storage"
+echo "copying..."
+gsutil -m cp -r /tmp/artifacts-$date_now gs://robot-shop-taurus
+echo "exited"
