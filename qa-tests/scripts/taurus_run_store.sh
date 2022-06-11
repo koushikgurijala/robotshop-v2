e9f587301b9d03cb473da4bc0e528036f543b201
csduvvuri <csduvvuri@gmail.com>
2022-06-12 04:53:37 +0530

    new

--- qa-tests/scripts/taurus_run_store.sh
+++ qa-tests/scripts/taurus_run_store.sh
@@ -6,4 +6,4 @@ mv /tmp/artifacts /tmp/artifacts-$date_now
 echo "Script for copying the artifacts to Cloud Storage"
 echo "copying..."
 gsutil -m cp -r /tmp/artifacts-$date_now gs://robot-shop-taurus
-echo "exited"
+echo "exited...s"
